8c3266044276b39d15635cc428f5ba536dab6948
余文清 <ywq19920427@163.com>
2018-08-05 12:19:22 +0800

    省市区控件实现

--- pages/userinfo/useredit.js
+++ pages/userinfo/useredit.js
@@ -1,72 +1,206 @@
 // pages/userinfo/useredit.js
 
-var _app = getApp()
+var app = getApp();
+ 
+var util = require('../../utils/util.js');
+var tcity = require("../../utils/citys.js");
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    sex: ['保密', '男', '女'],
+    provinces: [],
+    province: "",
+    citys: [],
+    city: "",
+    countys: [],
+    county: '',
+    value: [0, 0, 0],
+    values: [0, 0, 0],
+    condition: false
   },
+  bindChange: function (e) {
+  console.info(4567890);
+    //console.log(e);
+    var val = e.detail.value
+    var t = this.data.values;
+    var cityData = this.data.cityData;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    let that = this;
+    if (val[0] != t[0]) {
+      console.log('province no ');
+      const citys = [];
+      const countys = [];
+
+      for (let i = 0; i < cityData[val[0]].sub.length; i++) {
+        citys.push(cityData[val[0]].sub[i].name)
+      }
+      for (let i = 0; i < cityData[val[0]].sub[0].sub.length; i++) {
+        countys.push(cityData[val[0]].sub[0].sub[i].name)
+      }
+
+      this.setData({
+        province: this.data.provinces[val[0]],
+        city: cityData[val[0]].sub[0].name,
+        citys: citys,
+        county: cityData[val[0]].sub[0].sub[0].name,
+        countys: countys,
+        values: val,
+        value: [val[0], 0, 0],
+      })
+
+      return;
+    }
+    if (val[1] != t[1]) {
+      console.log('city no');
+      const countys = [];
+
+      for (let i = 0; i < cityData[val[0]].sub[val[1]].sub.length; i++) {
+        countys.push(cityData[val[0]].sub[val[1]].sub[i].name)
+      }
+
+      this.setData({
+        city: this.data.citys[val[1]],
+        county: cityData[val[0]].sub[val[1]].sub[0].name,
+        countys: countys,
+        values: val,
+        value: [val[0], val[1], 0]
+      })
+      return;
+    }
+    if (val[2] != t[2]) {
+      console.log('county no');
+      this.setData({
+        county: this.data.countys[val[2]],
+        values: val
+      })
+      return;
+    }
+
+  },
+  closeErr: function () { //关闭错误
+    //util.clearError(this);
+  },
+  open: function () {
+    this.setData({
+      condition: !this.data.condition
+    })
+  },
+  selectsex: function (e) {
+    this.setData({
+      'userInfo.gender': e.detail.value
+    })
+  },
+  dateAvatar: function () {
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths
+        wx.uploadFile({
+          url: 'https://xcx.codems.cn/api/upload',
+          filePath: tempFilePaths[0],
+          name: 'file',
+          formData: {
+            'user': app.globalData.userInfo.id
+          },
+          success: function (res) {
+            var data = JSON.parse(res.data);
+            console.log(data);
+            if (data.status == 1) {
+              that.setData({
+                'userInfo.avatarUrl': data.data
+              })
+              util.clearError(that);
+            } else {
+              console.log(data.msg);
+              util.isError(data.msg, that);
+            }
+          },
+          fail: function () {
+            util.isError('上传失败', that);
+          }
+        })
+      }
+    })
+  },
+  formSubmit: function (e) {
+    var that = this;
+    if (!(/^1[34578]\d{9}$/.test(e.detail.value.phone))) {
+      util.isError('手机号码错误', that);
+      return false;
+    }
+    this.setData({
+      'userInfo.province': this.data.province,
+      'userInfo.city': this.data.city,
+      'userInfo.county': this.data.county,
+      'userInfo.nickName': e.detail.value.nickName,
+      'userInfo.phone': e.detail.value.phone
+    })
+    wx.request({
+      url: 'https://xcx.codems.cn/api/user/editUser',
+      data: {
+        'userInfo': that.data.userInfo,
+        'sk': app.globalData.sk
+      },
+      method: 'POST',
+      header: {
+        'Content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res);
+        if (res.data.status == '1') {
+          util.clearError(that);
+          app.setUserInfo(res.data.user);
+          wx.navigateBack({
+            delta: 1
+          })
+        } else {
+          util.isError(res.data.msg, that);
+        }
+      },
+      fail: function (res) {
+        util.isError('修改失败', that);
+      }
+    })
+  },
+  onLoad: function () {
+    var that = this;
+
+ 
     that.setData({
-      userinfo: _app.globalData.userInfo
-    });
-    console.info(_app.globalData.userInfo);
-  },
+      userinfo: app.globalData.userInfo,
+      gender: '男',
+      'province':'福建',
+      'city': '厦门',
+      'county':'思明区'
+    })
+   
+    console.log(app.globalData.userInfo);
+    tcity.init(that);
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+    var cityData = that.data.cityData;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    const provinces = [];
+    const citys = [];
+    const countys = [];
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    for (let i = 0; i < cityData.length; i++) {
+      provinces.push(cityData[i].name);
+    }
+    console.log('省份完成');
+    for (let i = 0; i < cityData[0].sub.length; i++) {
+      citys.push(cityData[0].sub[i].name)
+    }
+    console.log('city完成');
+    for (let i = 0; i < cityData[0].sub[0].sub.length; i++) {
+      countys.push(cityData[0].sub[0].sub[i].name)
+    }
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    that.setData({
+      'provinces': provinces,
+      'citys': citys,
+      'countys': countys
+    })
+    console.log('初始化完成');
   }
 })
